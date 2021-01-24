8ce748e0f093733afade289689c6f7982bdc788a
Islam Rabie <eslamrbie@gmail.com>
2021-01-25 01:19:04 +0200

    Add Scriptable Objects, Add move functionality in PlayerManager

--- Assets/_Down/Scripts/_Player/PlayerStatusSO.cs
+++ Assets/_Down/Scripts/_Player/PlayerStatusSO.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStatusSO : ScriptableObject
+namespace SoulPlayer
 {
-    [SerializeField] public string _playerName;
-    [SerializeField] public int _playerLevel;
-    [SerializeField] public int _playerStep;
-    [SerializeField] public int _playerMoveSpeed;
-    [SerializeField] public int _playerDashSpeed;
+
+    [CreateAssetMenu]
+    public class PlayerStatusSO : ScriptableObject
+    {
+        [SerializeField] public string _playerName;
+        [SerializeField] public int _playerLevel;
+        [SerializeField] public int _playerStep;
+        [SerializeField] public int _playerMoveSpeed;
+        [SerializeField] public int _playerDashSpeed;
+    }
 }
