8151c5117ec51158f918587c34eeeeabde263055
Mohamed Saad <mohamedsaadamin.cis@gmail.com>
2021-01-24 18:42:21 +0200

    Implement horizontal parallax effect

---
+++ Assets/_Down/Scripts/ParallaxEffect.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+namespace _DownGame
+{
+    public class ParallaxEffect : MonoBehaviour
+    {
+        [SerializeField]
+        float speed;
+        [SerializeField]
+        float deltaPosition;
+        [SerializeField]
+        Vector2 startPosition;
+
+        Vector2 newPosition;
+
+        void Update()
+        {
+            newPosition.x = Mathf.Repeat(Time.time * speed, deltaPosition);
+            transform.position = startPosition + Vector2.left * newPosition;
+        }
+    }
+}
