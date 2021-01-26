fba49e2445459c44bb3ea6d4c395378643840492
Islam Rabie <eslamrbie@gmail.com>
2021-01-26 08:06:44 +0200

    Add Getters, Setters, variables

--- Assets/_Down/Scripts/_Player/IPlayerPowerUpEffect.cs
+++ Assets/_Down/Scripts/_Player/IPlayerPowerUpEffect.cs
@@ -7,8 +7,12 @@ namespace SoulPlayer
     internal abstract class IPlayerPowerUpEffect
     {
         protected int _numberOfPowerUps;
+        [SerializeField][Range(100, 5000)]protected int _dashTimeMS = 1000;
         internal abstract void Effect(MonoBehaviour behaviour);
         internal abstract int GetNumberOfPowerUps();
         internal abstract void AddPowerUp();
+        internal abstract int GetEffectTimeMS();
+        internal abstract void SetEffectTimeMS(int dashTimeMS);
+
     }
 }
