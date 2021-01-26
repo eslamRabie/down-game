c6c94923fb659281d1b4abeb756b48762d79db0f
Islam Rabie <eslamrbie@gmail.com>
2021-01-26 09:00:34 +0200

    Update PlayerCollisionHandler
    moved PlayerEnemyInteraction collision handler to PlayerCollisionHandler

--- Assets/_Down/Scripts/_Player/PlayerCollisionHandler.cs
+++ Assets/_Down/Scripts/_Player/PlayerCollisionHandler.cs
@@ -1,37 +1,26 @@
+using System;
 using UnityEngine;
 
 namespace SoulPlayer
 {
     internal class PlayerCollisionHandler: MonoBehaviour
     {
-        private Rigidbody2D _playerBody2D;
-        private Collider2D _playerCollider2D;
         private PlayerManager _playerManager;
         private PlayerMovement _playerMovement; 
         
         private string _leftWallTag = "LeftConfiner";
         private string _rightWallTag = "RightConfiner";
         
-        internal void AddBody2D(Rigidbody2D playerBody)
-        {
-            _playerBody2D = playerBody;
-        }
-
-        internal void AddCollider(Collider2D playerCollider)
-        {
-            _playerCollider2D = playerCollider;
-        }
-
-        void AddPlayer(Transform player)
-        {
-            _playerManager = player.GetComponent<PlayerManager>();
-            _playerBody2D = player.GetComponent<Rigidbody2D>();
-            _playerCollider2D = player.GetComponent<Collider2D>();
-            _playerMovement = player.GetComponent<PlayerMovement>();
-        }
         
+        private void Start()
+        {
+            _playerManager = GetComponent<PlayerManager>();
+            _playerMovement = GetComponent<PlayerMovement>();
+        }
+
         private void OnCollisionEnter2D(Collision2D other)
         {
+            Debug.Log("Collided");
             if (other.gameObject.CompareTag(_leftWallTag)) _playerMovement.LeftWallCollision(true);
             if (other.gameObject.CompareTag(_rightWallTag)) _playerMovement.RightWallCollision(true);
             if (other.gameObject.CompareTag("Disguise"))
